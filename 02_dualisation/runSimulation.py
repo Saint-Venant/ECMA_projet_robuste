772061e61999ca5ddf5e7cbbff2b89ffe32053e3
Matt <matthieu.roux92@gmail.com>
2020-01-27 01:40:20 +0100

    Finish to prepare backtest

--- 02_dualisation/runSimulation.py
+++ 02_dualisation/runSimulation.py
@@ -1,4 +1,5 @@
 import os
+import shutil
 
 data = os.getcwd() + '/../modified_data/'
 model = 'robuste2Mod.mod'
@@ -8,8 +9,10 @@ instances = os.listdir(data)
 commandFormat = 'oplrun.exe {} {}'
 
 for instance in instances:
+    print(instance)
     instanceName = data + instance
     #outputFileName = output + instance[:-4] + '_results.dat'
     command = commandFormat.format(model, instanceName)
     os.system(command)
+    shutil.move('output.dat', 'simulation2/' + instance[:-4] + '_results.dat')
     break
